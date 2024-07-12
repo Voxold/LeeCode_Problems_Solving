fc8213f5ba9df0d78a76c719ec8777855312b1e7
Voxold <bilalajaha@gmail.com>
2024-07-12 15:17:26 +0100

    Return the maximum number of pairs

---
+++ 0x00_String/2744. Find Maximum Number of String Pairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumNumberOfStringPairs(self, words: list[str]) -> int:
+        seen = set()
+        pairs = 0
+        for word in words:
+            reversed_word = word[::-1]
+            if reversed_word in seen:
+                pairs += 1
+                seen.remove(reversed_word)
+            else:
+                seen.add(word)
+        return pairs
