15326254b7dbf4ab2140e39f552b2f7201f93a8a
Voxold <bilalajaha@gmail.com>
2024-07-19 13:09:08 +0100

    Contains Duplicate

--- 0x01_Array/217. Contains Duplicate.py
+++ 0x01_Array/217. Contains Duplicate.py
@@ -1,7 +1,4 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         st = set(nums)
-        if len(nums) != len(st):
-            return True
-        else:
-            return False
+        return len(nums) != len(st)
